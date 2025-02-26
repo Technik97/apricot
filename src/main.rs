c405e3d048d507a32776c02b131f01d810476bac
technik <aniket.97.mane@gmail.com>
2025-02-26 21:07:21 +0530

    Added word enum to store Builtin and Userdefined words

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
+use std::collections::HashMap;
+
+enum Word {
+    BuiltIn(fn(&mut Forth)),
+    UserDefined(Vec<String>),
+}
+
 struct Forth {
     stack: Vec<i32>,
+    dictionary: HashMap<String, Word>,
 }
 
 impl Forth {
     fn new() -> Self {
-        Self { stack: Vec::new() }
+        let mut dictionary = HashMap::new();
+        dictionary.insert("+".to_string(), Word::BuiltIn(Forth::add));
+        dictionary.insert("dup".to_string(), Word::BuiltIn(Forth::dup));
+
+        Self {
+            stack: Vec::new(),
+            dictionary,
+        }
+    }
+
+    fn add(&mut self) {
+        if let (Some(a), Some(b)) = (self.pop(), self.pop()) {
+            self.push(a + b);
+        }
     }
 
     fn push(&mut self, value: i32) {
